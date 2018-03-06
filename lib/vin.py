4a8daea515ea167a17acb287f03b282b7548fb90
Greg Cope <gregcope@gmail.com>
2018-03-06 09:04:48 +0000

    Added pins and more logic

--- lib/vin.py
+++ lib/vin.py
@@ -3,24 +3,35 @@
 #
 
 # Usage
-# vin = Vin()
+# vin = Vin(VoltageDividerPin, regEngablePin)
 # vin.volts()
 # vin.regulatorOn()
 # vin.regulatorState()
+# etc ...
 
 class Vin:
 
-    def __init__(self):
+    def __init__(self, VoltageDividerPin, regEngablePin):
         self.regulatorState = False
-        self.py = py
+        self.VoltageDividerPin = VoltageDividerPin
+        self.regEngablePin = regEngablePin
 
     def volts(self):
+        # read ADC on VoltageDividerPin and work out voltage
         print('checking vin volts')
+
         _vinVolts = 0
+        # do stuff to VoltageDividerPin
         return _vinVolts
 
     def regulatorOn(self):
         print('Regulator On')
+        # set regEngablePin ON
+
+    def regulatorOff(self):
+        print('Regulator Off')
+        # set regEngablePin OFF
 
     def regulatorState(self):
         print('Regulator is: X')
+        # reaf regEngablePin state

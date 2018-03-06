b5dbd1c8701ba10d505a73ca1b7e092001119c4b
Greg Cope <gregcope@gmail.com>
2018-03-06 07:19:36 +0000

    Initial comit - boiler plate - does not actually work

---
+++ lib/vin.py
@@ -0,0 +1,26 @@
+#
+# A class to keep track of Voltage In
+#
+
+# Usage
+# vin = Vin()
+# vin.volts()
+# vin.regulatorOn()
+# vin.regulatorState()
+
+class Vin:
+
+    def __init__(self):
+        self.regulatorState = False
+        self.py = py
+
+    def volts(self):
+        print('checking vin volts')
+        _vinVolts = 0
+        return _vinVolts
+
+    def regulatorOn(self):
+        print('Regulator On')
+
+    def regulatorState(self):
+        print('Regulator is: X')

07dfaae322de7774f48fbc3442f275507910c202
Greg Cope <gregcope@gmail.com>
2018-03-07 07:17:53 +0000

    added BilgeSwitch class

---
+++ lib/bilgeSwitch.py
@@ -0,0 +1,20 @@
+#
+# A class to keep track of a bilgeSwitch
+#
+
+# Usage
+# bilgeSwitch = BilgeSwitch(bilgeSwitchPin)
+# bilgeSwitch.isO()
+
+class BilgeSwitch:
+
+    def __init__(self, bilgeSwitchPin):
+        self.bilgeSwitchPin = bilgeSwitchPin
+
+	def isOn(self):
+        print('is bilgeSwitch On')
+        # set regEngablePin ON
+		if True:
+		    return True
+		else
+		    return False

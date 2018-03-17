2e3d8a7f86808af0dd1bfb9d47e2f36e5165481b
Greg Cope <gregcope@gmail.com>
2018-03-17 10:24:53 +0000

    Rotation code

--- lib/sdCardFile.py
+++ lib/sdCardFile.py
@@ -2,6 +2,9 @@
 # A class to manage an sdcard log file
 #
 
+from machine import SD
+import OS
+
 # Usage
 # sdCardFile = SdCardFile('logfile')
 # sdCardFile.write("a string")
@@ -10,13 +13,34 @@
 
 class sdCardFile:
 
-    def __init__(self, FileName):
-        self._fileName = FileName
+    # 100K rotation size
+    ROTATIONSIZE = const(100000)
+
+    def __init__(self, fileName):
+        self._fileName = fileName
+        # mount
+        self._sd = SD()
+        os.mount(self._sd, '/sd')
+        # rotate if required
+        self.rotateIfNeeded(self)
+        # open file
+        self._f = open(self._fileName, 'w')
 
     def write(self, ThingToWrite):
         print(ThingToWrite)
-        # set self.ledPin ON
+        self._f.write("{}: {}\n".format(rtc.now(), ThingToWrite))
+        os.sync()
 
-    def read(self):
-        print('Reading')
-        # set self.ledPin OFF
+    def rotateIfNeeded(self):
+        # Check file size
+        # and rotate if above ROTATIONSIZE
+        stat = os.stat(self._fileName)
+        print("{} is {} bytes".format(self._fileName, stat[6))
+        if stat[6] > ROTATIONSIZE:
+            # make a new name
+            _newName = self._fileName + '.old'
+            # remove it if exists
+            try:
+                os.remove(_newName)
+            # rename
+            os.rename(self._fileName, _newName)        

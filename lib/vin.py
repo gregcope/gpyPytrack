d54684aa873605a3937689824d60272240c42437
Greg Cope <gregcope@gmail.com>
2018-04-15 09:29:27 +0100

    Setup pins

--- lib/vin.py
+++ lib/vin.py
@@ -14,10 +14,16 @@ class Vin:
     CHARGE_THRESHOLD = const(13)
     LOW_THRESHOLD = 11.7
 
-    def __init__(self, VoltageDividerPin, regEngablePin):
-        self.regulatorState = False
-        self.VoltageDividerPin = VoltageDividerPin
-        self.regEngablePin = regEngablePin
+    def __init__(self, voltageDividerPin, regEngablePin):
+        self.voltageDividerPin = voltageDividerPin
+
+        # setup the regulator pin
+        # as output to toggle the regulator
+        self.regEngablePin = Pin('regEngablePin', mode=Pin.OUT)
+
+        # setup the voltageDividerPin
+        # as input to read the ADC
+        self.voltageDividerPin = Pin('voltageDividerPin', mode=Pin.IN)
 
     def volts(self):
         # read ADC on VoltageDividerPin and work out voltage
@@ -28,13 +34,23 @@ class Vin:
         return vinVolts
 
     def regulatorOn(self):
-        print('Regulator On')
+        print('Regulator On
         # set regEngablePin ON
+        # Need to self.regEngablePin.hold(True) to keep it on
+        # over power downs
+        self.regEngablePin.value(1)
+        self.regEngablePin.hold(True)
 
     def regulatorOff(self):
         print('Regulator Off')
         # set regEngablePin OFF
+        # Need to self.regEngablePin.hold(False) to allow it to keep it on
+        # over power downs
+        self.regEngablePin.hold(False)
+        self.regEngablePin.value(0)
 
     def regulatorState(self):
         print('Regulator is: X')
-        # reaf regEngablePin state
+        # return state of pin
+        # true == on, false == 0ff
+        return self.regEngablePin()
